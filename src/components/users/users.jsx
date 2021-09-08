a387a46f90dba6f93553cc57c5db73bb5eaac66d
Renuka Schreck <schrer3@gmail.com>
2021-09-08 07:01:45 -0500

    New components cleanup

--- src/components/users/users.jsx
+++ src/components/users/users.jsx
@@ -1,9 +1,9 @@
 import React, { Fragment, useContext } from "react";
 import { users } from "./user-list";
 import { UserItem } from "./user-item";
-import ContactContext from "../../context/contact/contact-context";
+//import ContactContext from "../../context/contact/contact-context";
 export function Users() {
-  const contactContext = useContext(ContactContext);
+  //const contactContext = useContext(ContactContext);
   //const { contacts } = contactContext;
   return (
     <Fragment>
